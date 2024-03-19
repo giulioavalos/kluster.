c239a795de866bb7ccab2315c9802a7582d91338
Maximiliam Augusto <157996663+macsimacsi@users.noreply.github.com>
2024-03-19 14:34:08 -0300

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_19_153738) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_18_193206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "albums", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
-    t.text "content"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "album_id", null: false
-    t.index ["album_id"], name: "index_reviews_on_album_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
+  
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,6 +26,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_19_153738) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "reviews", "albums"
-  add_foreign_key "reviews", "users"
 end
