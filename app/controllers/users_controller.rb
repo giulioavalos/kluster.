245793f593a76607f1a9598bce9d9ccf2bb839bf
Maximiliam Augusto <157996663+macsimacsi@users.noreply.github.com>
2024-03-19 16:26:49 -0300

    added some functionality to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def callback
+    @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    if User.find_by(spotify_id: @spotify_user.spotify_id).nil?
+      @user = User.new
+      @user.spotify_id = @spotify_user.spotify_id
+      @user.name = @spotify_user
+      @user.save
+      Devise::login(@user)
+    else
+      @user = User.find_by(spotify_id: @spotify_user.spotify_id)
+      Devise::login(@user)
+    end
+  end
 end
+
+
+current_user.reviews
