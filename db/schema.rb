0e2161725b8d62c5fe5d897a2b2898cb747ad08a
Maximiliam Augusto <157996663+macsimacsi@users.noreply.github.com>
2024-03-19 14:36:09 -0300

    a

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.1].define(version: 2024_03_18_193206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,5 +25,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_18_193206) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
+  
 end
