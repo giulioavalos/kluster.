51b6734fbc6cafec7c29cfa275ea4f789eab495d
Gregoire Cazier <cazierge@gmail.com>
2024-03-19 17:44:51 -0300

    Here is my home page and routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
-  get 'reviews/create'
-  get 'reviews/show'
-  get 'reviews/edit'
-  get 'reviews/update'
-  get 'reviews/destroy'
   devise_for :users
   root to: "pages#home"
-  get '/search', to: 'pages#search', as: 'search'
-  get '/artists', to: 'artists#index', as: 'artists'
-  get 'artists/:id', to: 'artists#show', as: 'artist'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :reviews, except: [:index, :new]
+  resources :artists, only: [:index, :show]
+
+  get '/search', to: 'pages#search', as: 'search'
+
+  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :albums do
+    resources :reviews, only: [:create, :show, :edit, :update, :destroy]
+  end
 end
