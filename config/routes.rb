e33b221e5968e713a835f92cf37dc7a6133d7eee
Gregoire Cazier <cazierge@gmail.com>
2024-03-20 18:01:44 -0300

    here is my commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,24 @@ Rails.application.routes.draw do
   get '/auth/spotify/callback', to: 'users#spotify'
   root to: "pages#home"
 
-  resources :reviews, except: [:index, :new]
-  resources :artists, only: [:index, :show]
-
+  # Routes for searching
   get '/search', to: 'pages#search', as: 'search'
 
-  resources :artists do
-    resources :albums
+  # Routes for artists
+  resources :artists, only: [:index, :show]
+
+  # Routes for albums with nested favorites and likes
+  resources :albums do
+    resource :favorite, only: [:create, :destroy]
+    resource :like, only: [:create, :destroy]
+    resources :reviews, only: [:create, :show, :edit, :update, :destroy]
+  end
+
+  # Routes for reviews with nested likes
+  resources :reviews, except: [:index, :new] do
+    resource :like, only: [:create, :destroy]
   end
 
   # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :albums do
-    resources :reviews, only: [:create, :show, :edit, :update, :destroy]
-  end
 end
