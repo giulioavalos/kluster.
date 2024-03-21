3c4446972459ec9ea629d102085d90b0c46af2e6
giulioavalos <giulio_avalos@hotmail.es>
2024-03-21 11:17:48 -0300

    added-review-functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,27 @@ Rails.application.routes.draw do
 
   get '/search', to: 'pages#search', as: 'search'
 
-  resources :artists do
-    resources :albums
+  resources :artists, only: :show do
+    resources :reviews, only: [:create]
+  end
+
+  resources :albums, only: :show  do
+    resources :reviews, only: [:create]
+  end
+
+  resources :tracks, only: :show  do
+    resources :reviews, only: [:create]
   end
 
   # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :albums do
-    resources :reviews, only: [:create, :show, :edit, :update, :destroy]
-  end
+
+  get '/artists', to: 'artists#index'
+  get '/artists/:spotify_artist_id', to: 'artists#show'
+
+  get '/albums', to: 'albums#index'
+  get '/albums/:spotify_album_id', to: 'albums#show'
+
+  post '/albums/:spotify_album_id/reviews', to: 'reviews#create'
+
 end
