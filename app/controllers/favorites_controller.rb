60754db1076fa11c53057cf5face487bfc47dc80
Gregoire Cazier <cazierge@gmail.com>
2024-03-21 14:24:36 -0300

    here it is

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,9 +2,6 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    # album = Album.find(params[:album_id])
-    # current_user.favorite_albums << album
-
     Favorite.new(user: current_user, spotify_item_id: params[:album_id], spotify_item_type: "album").save
     redirect_to album_path(params[:album_id])
   end
@@ -14,6 +11,5 @@ class FavoritesController < ApplicationController
     album_id = favorite.spotify_item_id
     favorite.destroy
     redirect_to album_path(album_id)
-
   end
 end
