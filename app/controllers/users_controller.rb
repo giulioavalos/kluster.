fa88e3482eae68847dd3fcac8038b274857e512d
Maximiliam Augusto <157996663+macsimacsi@users.noreply.github.com>
2024-03-21 11:55:02 -0300

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,15 +14,11 @@ class UsersController < ApplicationController
       @user.country = @spotify_user.country
       @user.save
       sign_up @user
-      # :bypass is set to ignore devise related callbacks and only save the
-      # user into session.
       sign_i @user, :bypass => true
       redirect_to root_path
     else
       @user = User.find_by(spotify_id: @spotify_user.uri)
       sign_in @user
-      # :bypass is set to ignore devise related callbacks and only save the
-      # user into session.
       sign_in @user, :bypass => true
       redirect_to root_path
     end
