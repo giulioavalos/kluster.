5f0a6645b4f2cc2be8b6962ffe92348a5077dcc9
Maximiliam Augusto <157996663+macsimacsi@users.noreply.github.com>
2024-03-21 16:13:49 -0300

    deleted useless files (feed controller)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,9 @@
 class User < ApplicationRecord
   has_many :reviews
-<<<<<<< HEAD
   has_many :favorites
   has_many :favorite_albums, through: :favorites, source: :album
   serialize :avatars, JSON
 
-=======
-  has_many :feed_items
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
->>>>>>> 3f24e986f8f5f237c6a0c860a6e2a174b4a46af6
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
