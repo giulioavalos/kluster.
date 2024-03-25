2347fa84ba1cf9d8960548b7b3a85e41d5bb1f02
giulioavalos <giulio_avalos@hotmail.es>
2024-03-25 16:21:58 -0300

    changed-seeds-2

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,17 +36,4 @@ review3.update(spotify_item_id: spotify_album_ids.sample, spotify_item_type: 'al
 review4.update(spotify_item_id: spotify_album_ids.sample, spotify_item_type: 'album')
 review5.update(spotify_item_id: spotify_album_ids.sample, spotify_item_type: 'album')
 
-avatars = [
-  'https://i.pravatar.cc/150?img=1',
-  'https://i.pravatar.cc/150?img=2',
-  'https://i.pravatar.cc/150?img=3',
-  'https://i.pravatar.cc/150?img=4',
-  'https://i.pravatar.cc/150?img=5',
-  'https://i.pravatar.cc/150?img=6',
-  'https://i.pravatar.cc/150?img=7',
-  'https://i.pravatar.cc/150?img=8',
-  'https://i.pravatar.cc/150?img=9',
-  'https://i.pravatar.cc/150?img=10'
-]
-
 puts "Seeded!"
