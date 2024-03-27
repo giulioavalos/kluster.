ba7054c0dade39326e7e567859594716624522aa
giulioavalos <giulio_avalos@hotmail.es>
2024-03-26 21:33:18 -0300

    added-cards-for-artists

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,7 @@ class PagesController < ApplicationController
     if @search_query.present?
       @artists = RSpotify::Artist.search(@search_query)
       @albums = RSpotify::Album.search(@search_query)
+      @users = User.where("name LIKE ?", "%#{@search_query}%")
     end
   end
 end
